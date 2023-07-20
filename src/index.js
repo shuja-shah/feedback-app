d81f60061029210abca2287548a94a4c4a59107d
shuja-shah <shujashah786@gmail.com>
2023-07-21 00:26:02 +0500

    Initalize Frontend

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+import App from "./App";
+// import "./style.css";
+import store, { persistor } from "./redux/store";
+
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
+);
