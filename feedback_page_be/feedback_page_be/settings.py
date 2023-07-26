f530ccbb362c75a4e34673663f194197e01f600b
shuja-shah <shujashah786@gmail.com>
2023-07-26 17:16:40 +0500

    Set Up CORS policy

--- feedback_page_be/feedback_page_be/settings.py
+++ feedback_page_be/feedback_page_be/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "FbDkList",
     "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -57,6 +59,11 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
     ],
 }
+CORS_ALLOW_ALL_ORIGINS = True
+
+
+
+
 
 ROOT_URLCONF = "feedback_page_be.urls"
 
