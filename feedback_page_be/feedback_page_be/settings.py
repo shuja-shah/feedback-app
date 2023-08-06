7311f0d1abc327af8903bd5254c192bfb59e2fda
shuja-shah <shujashah786@gmail.com>
2023-08-06 23:34:06 +0500

    Finalize

--- feedback_page_be/feedback_page_be/settings.py
+++ feedback_page_be/feedback_page_be/settings.py
@@ -90,9 +90,16 @@ WSGI_APPLICATION = "feedback_page_be.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'mssql',
+        'NAME': 'feedback',  # Replace with your SQL Server database name
+        'USER': 'admin',  # Replace with your SQL Server database username
+        'PASSWORD': 'admin',  # Replace with your SQL Server database password
+        'HOST': 'localhost',  
+        'PORT': '1433',  
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server', 
+        },
     }
 }
 
